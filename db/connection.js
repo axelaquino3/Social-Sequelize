6395fcd529a1d4ad83b5e531e3ded5147913d37d
axelaquino3 <axelaquino@google.com>
2023-10-11 14:33:19 -0400

    Updated Database & Models

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,14 @@
+const path = require("path")
+const Sequelize = require("sequelize")
 
+const db = new Sequelize({
+    dialect: "sqlite",
+    storage: path.join(__dirname, "db.sqlite")
+})
 
-
+console.log(db)
 
 
 module.exports = {
     db,
-    
 }
--- models/Comment.js
+++ models/Comment.js
@@ -1,5 +1,14 @@
+const { db, DataTypes, Model } = require("../db/connection.js")
 
-let Comment;
+class Comment extends Model {};
+
+Comment.init({
+    body: DataTypes.STRING,
+    createdAt: DataTypes.STRING,
+}, {
+    sequelize: db,
+    modelName: "Comment"
+})
 
 
 module.exports = Comment;
